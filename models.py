7d5a6a14f660b7ab223cc79aeabd301e4e2263ba
erick <evargas44@ucmerced.edu>
2023-05-01 19:41:17 -0700

    created database and populated tables

---
+++ models.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+# Flask and SQLDatabase configuaration
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
+db = SQLAlchemy(app)
+
+# Database Models
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String,unique=True, nullable=False)
+    password = db.Column(db.String,unique = True, nullable=False)
+    # profile_pic = db.Column(db.String)
+    
+    def __init__(self, id, name, email, password) :
+        super().__init__()
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
+
+class Posts(db.Model):
+    __tablename__ = 'posts'
+
+    numkey = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_header = db.Column(db.String)
+    post_body = db.Column(db.String)
+
+    def __init__(self, numkey,user_id, header, body):
+        super().__init__()
+        self.numkey = numkey
+        self.user_id = user_id
+        self.post_header = header
+        self.post_body = body
+
+
+
+
+
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+
+    numkey = db.Column(db.Integer, primary_key=True)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_numkey = db.Column(db.Integer, db.ForeignKey('posts.numkey'))
+    body = db.Column(db.String)
+    
+
+    def __init__(self, numkey,userid,post_numkey, body) :
+        super().__init__()
+        self.numkey = numkey
+        self.users_id=userid
+        self.post_numkey = post_numkey
+        self.body = body
+
+class Likes(db.Model):
+    __tablename__ = 'likes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment_numkey = db.Column(db.Integer, db.ForeignKey('comments.numkey'))
+    post_numkey= db.Column(db.Integer, db.ForeignKey('posts.numkey'))
+    
+
+    def __init__(self, id,commentNumkey,postnumkey):
+        super().__init__()
+        self.id = id
+        self.comment_numkey = commentNumkey
+        self.post_numkey = postnumkey
+        
+if __name__ == '__main__':
+    db.drop_all()
+    db.create_all()
